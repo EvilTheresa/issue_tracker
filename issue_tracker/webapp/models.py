a79e4a184ff375a1459cabd4d37db7becc108f53
helux <kled.master@gmail.com>
2024-07-10 22:24:21 +0600

    Этап 2 завершен

--- issue_tracker/webapp/models.py
+++ issue_tracker/webapp/models.py
@@ -19,10 +19,29 @@ class Task(models.Model):
     summary = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    # type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    types = models.ManyToManyField(
+        Type,
+        related_name="tasks",
+        verbose_name="Типы",
+        blank=True,
+        through='TaskType',
+        through_fields=("task", "type"),
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+ #   type = models.ManyToManyField("webapp.Type", related_name="tasks", verbose_name="Типы", blank=True)
+
 
     def __str__(self):
         return self.summary
 
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+class TaskType(models.Model):
+    task = models.ForeignKey('Task', related_name='types_tasks', on_delete=models.CASCADE, )
+    type = models.ForeignKey('Type', related_name='tasks_types', on_delete=models.CASCADE, )
