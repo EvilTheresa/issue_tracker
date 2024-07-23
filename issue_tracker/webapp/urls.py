a4b9761d4ff83d517f7970f06b504d6149213c7f
helux <kled.master@gmail.com>
2024-07-24 04:13:07 +0600

    Поправлены урлы

--- issue_tracker/webapp/urls.py
+++ issue_tracker/webapp/urls.py
@@ -3,13 +3,12 @@ from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView,
 from .views.projects import ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='task_list'),
     path('project/<int:pk>/task/add/', TaskCreateView.as_view(), name='add_task'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='detail_task'),
     path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='update_task'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='delete_task'),
 
-    path('project/', ProjectListView.as_view(), name='project_list'),
+    path('', ProjectListView.as_view(), name='project_list'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('project/add/', ProjectCreateView.as_view(), name='project_add'),
     path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
--- issue_tracker/webapp/views/tasks.py
+++ issue_tracker/webapp/views/tasks.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView, ListView, CreateView, DeleteView
+from django.views.generic import TemplateView,  ListView, CreateView, DeleteView, UpdateView
 
 from ..models import Task, Project
 from ..forms import TaskForm
@@ -24,13 +24,6 @@ class TaskDetailView(TemplateView):
         return context
 
 
-# class TaskCreateView(FormView):
-#     template_name = "tasks/add_task.html"
-#     form_class = TaskForm
-#
-#     def form_valid(self, form):
-#         task = form.save()
-#         return redirect("detail_task", pk=task.pk)
 class TaskCreateView(CreateView):
     template_name = "tasks/add_task.html"
     form_class = TaskForm
@@ -42,46 +35,15 @@ class TaskCreateView(CreateView):
         task.save()
         return redirect("project_detail", pk=self.kwargs['pk'])
 
-# class TaskCreateView(CreateView):
-#     model = Task
-#     form_class = TaskForm
-#     template_name = "partial/form_task.html"
-#
-#     def form_valid(self, form):
-#         form.instance.project_id = self.kwargs['pk']
-#         return super().form_valid(form)
-#
-#     def get_success_url(self):
-#         return reverse_lazy('project_detail', kwargs={'pk': self.object.project.pk})
 
-
-class TaskUpdateView(FormView):
-    template_name = "tasks/update_task.html"
+class TaskUpdateView(UpdateView):
+    model = Task
     form_class = TaskForm
+    template_name = "tasks/update_task.html"
+    context_object_name = 'task'
 
-    def dispatch(self, request, *args, **kwargs):
-        self.task = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_object(self):
-        return get_object_or_404(Task, pk=self.kwargs.get("pk"))
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['instance'] = self.task
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = self.task
-        return context
-
-    def form_valid(self, form):
-        form.save()
-        return redirect("detail_task", pk=self.task.pk)
-
-    def form_invalid(self, form):
-        return self.render_to_response(self.get_context_data(form=form))
+    def get_success_url(self):
+        return reverse_lazy('project_detail', kwargs={'pk': self.object.project.pk})
 
 
 class TaskDeleteView(DeleteView):
