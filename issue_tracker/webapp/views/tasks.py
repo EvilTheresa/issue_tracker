c7b8f084c9302de19b7e0e7edf45a591af5f820d
helux <kled.master@gmail.com>
2024-07-25 17:56:07 +0600

    Дамп юзера admin admin

--- issue_tracker/webapp/views/tasks.py
+++ issue_tracker/webapp/views/tasks.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView,  ListView, CreateView, DeleteView, UpdateView
