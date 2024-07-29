0b2d25373800b7bccacec6757792d01fc8fab83b
helux <kled.master@gmail.com>
2024-07-29 18:13:41 +0600

    Регистрация скатана с артиклсов

--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from accounts.forms import MyUserCreationForm
+
+User = get_user_model()
+
 
 # Create your views here.
+class RegistrationView(CreateView):
+    form_class = MyUserCreationForm
+    template_name = "templates/registration.html"
+    model = User
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:articles')
+        return next_url
